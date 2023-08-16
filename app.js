313d663e3c609d98f80209c4d91b927470ca0692
Damien Subramanian <damiensoobz@gmail.com>
2023-08-16 16:35:44 +0200

    Create JS code to randomly sort tiles and create the tile grid

--- app.js
+++ app.js
@@ -1,6 +1,64 @@
 const cardArray = [
     {
         name: 'fries',
-        image: '',
+        image: 'images\fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        image: 'images\cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        image: 'images\hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        image: 'images\ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        image: 'images\milkshake.png'
+    },
+    {
+        name: 'pizza',
+        image: 'images\pizza.png'
+    },
+    {
+        name: 'fries',
+        image: 'images\fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        image: 'images\cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        image: 'images\hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        image: 'images\ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        image: 'images\milkshake.png'
+    },
+    {
+        name: 'pizza',
+        image: 'images\pizza.png'
     }
 ];
+
+cardArray.sort(() => 0.5 - Math.random()); /* Randomly sorts the array */
+const gridDisplay = document.querySelector('#grid') /* looks through the document for the specified element */
+
+function createBoard() {
+    for (let i = 0; i < 10; i++) {
+        const card = document.createElement('img');
+        card.setAttribute('src', 'images\blank.png');
+        card.setAttribute('data-id', i);
+        gridDisplay.append(card);
+    }
+};
+
+createBoard();
