513ced5d86456765a2079da3c1310046b780cd48
Damien Subramanian <damiensoobz@gmail.com>
2023-08-16 16:56:19 +0200

    Fixed an issue with a loop that caused the page to crash

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ cardArray.sort(() => 0.5 - Math.random()); /* Randomly sorts the array */
 const gridDisplay = document.querySelector('#grid') /* looks through the document for the specified element */
 
 function createBoard() {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img');
         card.setAttribute('src', 'images/blank.png');
         card.setAttribute('data-id', i);
